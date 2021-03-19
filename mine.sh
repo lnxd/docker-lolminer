aa912767c685d29846ea7d9813f46858375e7aff
lnxd <48756329+lnxd@users.noreply.github.com>
2021-03-19 15:50:15 +1100

    Removed Card Presence Test due to false negatives

--- mine.sh
+++ mine.sh
@@ -8,15 +8,6 @@ echo ""
 echo "Wallet:  $WALLET"
 echo "Pool:    $POOL"
 echo ""
-if test -f "/dev/dri"; then
-    echo "Card presence test passed."
-else 
-	echo "Card presence test failed. Please ensure you have passed your GPU to this container correctly."
-	echo ""
-	echo "Exiting due to error."
-	exit
-fi
-echo ""
 echo "Starting PhoenixMiner 5.5c with the following arguments:"
 echo "-pool $POOL -wal $WALLET.$PASSWORD -tt $TT  -tstop $TSTOP -tstart $TSTART -cdm 1 -cdmport 5450 $ADDITIONAL"
 
