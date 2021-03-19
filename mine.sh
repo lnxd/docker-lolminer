b298ff31bc9a7b92207f32a4c190a5610cdee942
lnxd <48756329+lnxd@users.noreply.github.com>
2021-03-19 14:48:50 +1100

    Wording update to mine.sh

--- mine.sh
+++ mine.sh
@@ -2,6 +2,7 @@
 
 echo "Project: PhoenixMiner 5.5c"
 echo "Author:  lnxd"
+echo "Base:    Ubuntu 20.04"
 echo "Target:  Unraid 6.9.0 - 6.9.1"
 echo ""
 echo "Wallet:  $WALLET"
@@ -16,7 +17,7 @@ else
 	exit
 fi
 echo ""
-echo "Starting PhoenixMiner 5.5c with arguments:"
+echo "Starting PhoenixMiner 5.5c with the following arguments:"
 echo "-pool $POOL -wal $WALLET.$PASSWORD -tt $TT  -tstop $TSTOP -tstart $TSTART -cdm 1 -cdmport 5450 $ADDITIONAL"
 
 cd /home/docker/phoenixminer
