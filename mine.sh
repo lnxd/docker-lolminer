1fb1e180e7d0821642992cbe9fa394473961b521
lnxd <48756329+lnxd@users.noreply.github.com>
2021-03-19 14:37:37 +1100

    Stripped testing variables from mine.sh

--- mine.sh
+++ mine.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-POOL="asia1.ethermine.org:4444"
-WALLET="0xe037C6245910EbBbA720514835d31B385D76927f"
-PASSWORD="x"
-TT="56"
-TSTOP="85"
-TSTART="80"
-ADDITIONAL="-amd "
-
 echo "Project: PhoenixMiner 5.5c"
 echo "Author:  lnxd"
 echo "Target:  Unraid 6.9.0 - 6.9.1"
