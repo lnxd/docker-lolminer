c8de692520414ba75fa821d0b47988f2bc53a37b
lnxd <48756329+lnxd@users.noreply.github.com>
2021-03-19 13:55:50 +1100

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-./home/docker/phoenixminer/PhoenixMiner -pool $POOL -wal $WALLET.$PASSWORD -tt $TT  -tstop $TSTOP -tstart $TSTART -cdm 1 -cdmport 5450 $ADDITIONAL
+cd /home/docker/phoenixminer
+./PhoenixMiner -pool $POOL -wal $WALLET.$PASSWORD -tt $TT  -tstop $TSTOP -tstart $TSTART -cdm 1 -cdmport 5450 $ADDITIONAL
