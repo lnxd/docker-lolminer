c2cc889cacba5aa21d4bab705f9fbcaf72424d75
lnxd <48756329+lnxd@users.noreply.github.com>
2021-03-24 22:36:47 +1100

    Testing change to sudo

--- mine.sh
+++ mine.sh
@@ -12,4 +12,4 @@ echo "Starting PhoenixMiner 5.5c with the following arguments:"
 echo "-pool $POOL -wal $WALLET.$PASSWORD -tt $TT  -tstop $TSTOP -tstart $TSTART -cdm 1 -cdmport 5450 $ADDITIONAL"
 
 cd /home/docker/phoenixminer
-./PhoenixMiner -pool $POOL -wal $WALLET.$PASSWORD -tt $TT  -tstop $TSTOP -tstart $TSTART -cdm 1 -cdmport 5450 $ADDITIONAL
+sudo PhoenixMiner -pool $POOL -wal $WALLET.$PASSWORD -tt $TT  -tstop $TSTOP -tstart $TSTART -cdm 1 -cdmport 5450 $ADDITIONAL
