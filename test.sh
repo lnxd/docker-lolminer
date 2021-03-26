ea66decc06c9fac8f9ce93e181f3fa5a5700ac4d
lnxd <48756329+lnxd@users.noreply.github.com>
2021-03-26 19:13:22 +1100

    Corrected typo in test.sh

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ docker stop phoenixminer
 docker rm phoenixminer
 docker build . -t phoenixminer
 docker run -d -e TZ="Australia/Melbourne" -p 127.0.0.1:5450:5450/tcp --name="phoenixminer" phoenixminer
-docker logs xmrig -f
+docker logs phoenixminer -f
