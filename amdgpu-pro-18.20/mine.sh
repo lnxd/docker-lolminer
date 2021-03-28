84e6eca253120beeaefce5ec8cc9445ad820f33b
lnxd <48756329+lnxd@users.noreply.github.com>
2021-03-28 17:34:58 +1100

    Made mine.sh work off variables for init message

--- amdgpu-pro-18.20/mine.sh
+++ amdgpu-pro-18.20/mine.sh
@@ -8,7 +8,7 @@ echo ""
 echo "Wallet:  $WALLET"
 echo "Pool:    $POOL"
 echo ""
-echo "Starting PhoenixMiner 5.5c with the following arguments:"
+echo "Starting PhoenixMiner $MINERV with the following arguments:"
 echo "-pool $POOL -wal $WALLET.$PASSWORD -tt $TT  -tstop $TSTOP -tstart $TSTART -cdm 1 -cdmport 5450 $ADDITIONAL"
 
 cd /home/docker/phoenixminer
