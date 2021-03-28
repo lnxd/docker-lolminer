43813832a7e128000fb052360f6f1aebf5b917c0
lnxd <48756329+lnxd@users.noreply.github.com>
2021-03-28 19:10:52 +1100

    Corrected docker build syntax

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 
 docker stop phoenixminer
 docker rm phoenixminer
-#docker build -f radeon-15.12.Dockerfile -t phoenixminer
-#docker build -f amdgpu-pro-18.20.Dockerfile -t phoenixminer
-docker build -f amdgpu-pro-20.20.Dockerfile -t phoenixminer
+#docker build -f radeon-15.12.Dockerfile . -t phoenixminer
+#docker build -f amdgpu-pro-18.20.Dockerfile . -t phoenixminer
+docker build -f amdgpu-pro-20.20.Dockerfile . -t phoenixminer
 docker run -d -e TZ="Australia/Melbourne" -p 127.0.0.1:5450:5450/tcp --name="phoenixminer" phoenixminer
 docker logs phoenixminer -f
