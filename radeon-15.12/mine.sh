3dff34fd53570ce54d99454b5b2b65ed66287228
lnxd <48756329+lnxd@users.noreply.github.com>
2021-03-28 17:40:06 +1100

    Prepared for pull

--- radeon-15.12/mine.sh
+++ radeon-15.12/mine.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "Project: PhoenixMiner 5.5c"
+echo "Project: PhoenixMiner $MINERV"
 echo "Author:  lnxd"
-echo "Base:    Ubuntu 14.04.02"
+echo "Base:    $BASE"
 echo "Target:  Unraid 6.9.0 - 6.9.1"
 echo ""
 echo "Wallet:  $WALLET"
 echo "Pool:    $POOL"
 echo ""
-echo "Starting PhoenixMiner 5.5c with the following arguments:"
+echo "Starting PhoenixMiner $MINERV with the following arguments:"
 echo "-pool $POOL -wal $WALLET.$PASSWORD -tt $TT  -tstop $TSTOP -tstart $TSTART -cdm 1 -cdmport 5450 $ADDITIONAL"
 
 cd /home/docker/phoenixminer
